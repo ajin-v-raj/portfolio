8aebf7b4ce2e5d365591354f84e6fb2e5098631d
ajin-v-raj <ajinvrj@gmail.com>
2023-12-04 12:25:46 +0530

    Edited

--- script.js
+++ script.js
@@ -5,46 +5,38 @@
   var detailTxtElement1 = document.getElementById('detail_txt1');
 
   detailsElement.addEventListener('mouseover', function() {
-    // Show the detail_txt element when details is hovered
     detailTxtElement.style.display = 'block';
   });
 
   detailsElement.addEventListener('mouseout', function() {
-    // Hide the detail_txt element when mouse leaves details
     detailTxtElement.style.display = 'none';
 
     
   });
   if (window.innerWidth <= 1024) {
     window.addEventListener('scroll', function() {
-        // Hide the detail_txt element when scrolling on small screens
         detailTxtElement.style.display = 'block';
   });
 }
   detailsElement1.addEventListener('mouseover', function() {
-    // Show the detail_txt element when details is hovered
     detailTxtElement1.style.display = 'block';
   });
 
   detailsElement1.addEventListener('mouseout', function() {
-    // Hide the detail_txt element when mouse leaves details
     detailTxtElement1.style.display = 'none';
 
     
   });
   if (window.innerWidth <= 1024) {
     window.addEventListener('scroll', function() {
-        // Hide the detail_txt element when scrolling on small screens
         detailTxtElement1.style.display = 'block';
   });
 }
 
 
 
-  // Get all progress bars
   var progressBars = document.querySelectorAll('.progress-bar');
 
-  // Function to check if an element is in the viewport
   function isInViewport(element) {
       var rect = element.getBoundingClientRect();
       return (
@@ -55,22 +47,19 @@
       );
   }
 
-  // Function to handle scroll event
   function handleScroll() {
       progressBars.forEach(function (progressBar) {
           if (isInViewport(progressBar)) {
               progressBar.classList.add('animated');
-              progressBar.style.opacity = 1; // Show the progress bar
+              progressBar.style.opacity = 1; 
           }
       });
 
-      // Remove the event listener after the animation has been applied to all progress bars
       if (document.querySelectorAll('.animated').length === progressBars.length) {
           window.removeEventListener('scroll', handleScroll);
       }
   }
 
-  // Add scroll event listener
   window.addEventListener('scroll', handleScroll);
 
 
